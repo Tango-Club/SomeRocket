e5ca0c7a97d5c6b94745eb51404dc64f8e80783a
Pxl <952130278@qq.com>
2021-09-27 01:12:46 +0800

    tmp

--- src/main/java/io/openmessaging/MessageBuffer.java
+++ src/main/java/io/openmessaging/MessageBuffer.java
@@ -20,7 +20,7 @@ public class MessageBuffer {
 		if (!cacheMap.containsKey(queueId)) {
 			try {
 				String cachePath;
-				if ((queueId % 3) == 0)
+				if ((queueId % 7) <= 1)
 					cachePath = Common.runDir + "/essd/cache";
 				else
 					cachePath = Common.runDir + "/pmem/cache";
