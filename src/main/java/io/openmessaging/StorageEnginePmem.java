1946ef13239213e808b0b8a2d2db27b70f316627
Pxl <952130278@qq.com>
2021-10-01 14:26:49 +0800

    tmp

--- src/main/java/io/openmessaging/StorageEnginePmem.java
+++ src/main/java/io/openmessaging/StorageEnginePmem.java
@@ -33,9 +33,7 @@ public class StorageEnginePmem extends StorageEngine {
 
 	@Override
 	public long write(ByteBuffer buffer) {
-		logger.info("write" + buffer);
-		MemoryBlock block = Common.heap.allocateMemoryBlock(buffer.capacity(), false);
-		Common.heap.setRoot(block.handle());
+		MemoryBlock block = Common.heap.allocateMemoryBlock(buffer.remaining(), false);
 		block.copyFromArray(buffer.array(), 0, 0, buffer.capacity());
 		blocks.add(block);
 		return blocks.size() - 1;
