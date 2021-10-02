1d6e9d41ce7bdeb57d56d9bd61ba5dbbfd4f5d4c
Pxl <952130278@qq.com>
2021-10-02 14:16:58 +0800

    tmp

--- src/main/java/io/openmessaging/MessageBuffer.java
+++ src/main/java/io/openmessaging/MessageBuffer.java
@@ -21,11 +21,11 @@ public class MessageBuffer {
 			String cachePath;
 			String offsetPath;
 			int rd = queueId % 100;
-			if (rd <= 21) {
+			if (rd <= 20) {
 				cachePath = Common.runDir + "/essd/cache";
 				offsetPath = Common.runDir + "/pmem/cache";
 				cacheMap.put(queueId, new StorageEngineEssd(topic, queueId, cachePath, offsetPath));
-			} else if (rd <= 22) {
+			} else if (rd <= 23) {
 				cacheMap.put(queueId, new StorageEngineDdr());
 			} else {
 				cacheMap.put(queueId, new StorageEnginePmem());
