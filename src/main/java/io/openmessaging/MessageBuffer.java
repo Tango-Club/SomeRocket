41d318f50090a66cc078038dad3881665abd585b
Pxl <952130278@qq.com>
2021-10-07 16:07:25 +0800

    tmp

--- src/main/java/io/openmessaging/MessageBuffer.java
+++ src/main/java/io/openmessaging/MessageBuffer.java
@@ -21,13 +21,13 @@ public class MessageBuffer {
 			String cachePath;
 			String offsetPath;
 			int rd = queueId % 100;
-			if (rd <= 20) {
+			if (rd <= 19) {
 				cachePath = Common.runDir + "/essd/cache";
 				offsetPath = Common.runDir + "/pmem/cache";
 				cacheMap.put(queueId, new StorageEngineEssd(topic, queueId, cachePath, offsetPath));
-			} else if (rd <= 24) {
+			} else if (rd <= 23) {
 				cacheMap.put(queueId, new StorageEngineDdr(false));
-			} else if (rd <= 25) {
+			} else if (rd <= 24) {
 				cacheMap.put(queueId, new StorageEngineDdr(true));
 			} else {
 				cacheMap.put(queueId, new StorageEnginePmem());
