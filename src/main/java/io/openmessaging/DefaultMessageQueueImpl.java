a144238eefc4a6ed75dca07bf0de8796ec7eadef
Pxl <952130278@qq.com>
2021-10-13 23:48:00 +0800

    tmp

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -138,7 +138,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
 			e.printStackTrace();
 		}
 		if (lastFlush < now && backup.dataNumber == now) {
-			int times = 100;
+			int times = 10;
 			while (times > 0 && lastFlush + 50 > backup.dataNumber && backup.dataNumber == now) {
 				try {
 					TimeUnit.MICROSECONDS.sleep(100);
